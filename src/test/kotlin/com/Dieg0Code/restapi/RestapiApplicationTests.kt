ad874cd9d1bb6a80fa8b66e2fcd6e739d1059f09
Diego Obando <diegoobando20@gmail.com>
2021-04-26 02:22:59 -0400

    Kotlin restapi

---
+++ src/test/kotlin/com/Dieg0Code/restapi/RestapiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.Dieg0Code.restapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RestapiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
