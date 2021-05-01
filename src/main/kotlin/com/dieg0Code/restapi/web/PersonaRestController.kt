7defd8d2e2b774e8aa5c0629422531e57852c041
Diego Obando <diegoobando20@gmail.com>
2021-05-01 05:25:52 -0400

    RestController added

---
+++ src/main/kotlin/com/dieg0Code/restapi/web/PersonaRestController.kt
@@ -0,0 +1,73 @@
+package com.dieg0Code.restapi.web
+
+import com.dieg0Code.restapi.business.IPersonaBusiness
+import com.dieg0Code.restapi.exception.BusinessException
+import com.dieg0Code.restapi.exception.NotFoundException
+import com.dieg0Code.restapi.model.Persona
+import com.dieg0Code.restapi.utils.Constants
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RestController // sirve para mapear los datos y mandarlos por la respuesta cuando se hace un peticion
+@RequestMapping(Constants.URL_BASE_PERSONAS)
+class PersonaRestController {
+    @Autowired
+    val personaBusiness: IPersonaBusiness? = null
+
+    @GetMapping("")
+    fun list(): ResponseEntity<List<Persona>> {
+        return try {
+            ResponseEntity(personaBusiness!!.list(), HttpStatus.OK)
+        } catch (e: Exception) {
+            ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR)
+        }
+    }
+
+    @GetMapping("/{id}")
+    fun load(@PathVariable("id") idPersona: Long): ResponseEntity<Persona> {
+        return try {
+            ResponseEntity(personaBusiness!!.load(idPersona), HttpStatus.OK)
+        } catch (e: BusinessException) {
+            ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR)
+        } catch (e: NotFoundException) {
+            ResponseEntity(HttpStatus.NOT_FOUND)
+        }
+    }
+
+    @PostMapping("")
+    fun insert(@RequestBody persona: Persona): ResponseEntity<Any> {
+        return try {
+            personaBusiness!!.save(persona)
+            val responseHeader = HttpHeaders()
+            responseHeader.set("location", Constants.URL_BASE_PERSONAS + "/" + persona.id)
+            ResponseEntity(responseHeader, HttpStatus.CREATED)
+        } catch (e: BusinessException) {
+            ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR)
+        }
+    }
+
+    @PutMapping("")
+    fun update(@RequestBody persona: Persona): ResponseEntity<Any> {
+        return try {
+            personaBusiness!!.save(persona)
+            ResponseEntity(HttpStatus.OK)
+        } catch (e: BusinessException) {
+            ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR)
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    fun delete(@PathVariable("id") idPersona: Long): ResponseEntity<Any> {
+        return try {
+            personaBusiness!!.remove(idPersona)
+            ResponseEntity(HttpStatus.OK)
+        } catch (e: BusinessException) {
+            ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR)
+        } catch (e: NotFoundException) {
+            ResponseEntity(HttpStatus.NOT_FOUND)
+        }
+    }
+}
